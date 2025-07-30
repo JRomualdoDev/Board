f48ce518bdc862c334179e136c29807d0c01b995
JoseRomualdo <jromualdo3@hotmail.com>
2025-07-30 09:30:56 -0300

    feat: new features and corrections

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,13 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.liquibase:liquibase-core:4.29.1")
+    implementation("com.mysql:mysql-connector-j:8.4.0")
+    implementation("org.projectlombok:lombok:1.18.34")
+    implementation("javax.xml.bind:jaxb-api:2.3.1") // Required for Liquibase
+    implementation("org.yaml:snakeyaml:2.2") // If using YAML changelogs
+
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
 }
 
 tasks.test {
